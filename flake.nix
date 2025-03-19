0b2e0e83647cb8cc98b89a1c42e28968907854f8
Semyon Protas <semyon.protas@gmail.com>
2025-03-19 03:31:05 +0300

    init commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Dracula Cursor Theme for NixOS";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: 
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      packages.${system}.dracula-cursors = pkgs.stdenv.mkDerivation {
+        pname = "dracula-cursors";
+        version = "1.0";
+
+        src = ./Dracula-cursors;  # Ensure this path is correct
+
+        installPhase = ''
+          mkdir -p $out/share/icons
+          cp -r $src $out/share/icons/dracula-cursors
+        '';
+
+        meta = {
+          description = "Dracula Cursor Theme";
+          license = pkgs.lib.licenses.mit;
+          platforms = pkgs.lib.platforms.linux;
+        };
+      };
+    };
+}
